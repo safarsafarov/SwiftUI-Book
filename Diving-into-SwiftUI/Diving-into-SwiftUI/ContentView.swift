d398e0ba95d33b807756e6245e914e94959240f6
safarsafarov <safarov.safar@hotmail.com>
2021-02-14 22:21:43 +0800

    commit

--- Diving-into-SwiftUI/Diving-into-SwiftUI/ContentView.swift
+++ Diving-into-SwiftUI/Diving-into-SwiftUI/ContentView.swift
@@ -8,14 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+  var body: some View {
+    Text("Hello World")
+  }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+          .previewLayout(.fixed(width: 568, height: 320))
     }
 }

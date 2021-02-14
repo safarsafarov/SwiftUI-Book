4d352fb6f255b7d590a8e221fa29020d983f5c7d
safarsafarov <safarov.safar@hotmail.com>
2021-02-14 22:20:02 +0800

    commit

---
+++ Diving-into-SwiftUI/Diving-into-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Diving-into-SwiftUI
+//
+//  Created by codestorm on 2021/2/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Diving-into-SwiftUI/Diving-into-SwiftUI/Diving_into_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Diving_into_SwiftUIApp.swift
+//  Diving-into-SwiftUI
+//
+//  Created by codestorm on 2021/2/17.
+//
+
+import SwiftUI
+
+@main
+struct Diving_into_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

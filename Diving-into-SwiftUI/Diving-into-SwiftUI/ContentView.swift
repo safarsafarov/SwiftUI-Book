35a4444a82301ae7582dcff112da2a308de21f24
safarsafarov <safarov.safar@hotmail.com>
2021-02-15 09:12:23 +0800

    commit

--- Diving-into-SwiftUI/Diving-into-SwiftUI/ContentView.swift
+++ Diving-into-SwiftUI/Diving-into-SwiftUI/ContentView.swift
@@ -8,20 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
-  var body: some View {
-    HStack {
-      VStack {
-        Color(red: 0.5, green: 0.5, blue: 0.5)
-        Text("Match this color")
-      }
-      VStack {
-        Color(red: 0.5, green: 0.5, blue: 0.5)
-        Text("R: 127  G: 127  B: 127")
-      }
+    var body: some View {
+        HStack {
+            VStack {
+                Color(red: 0.5, green: 0.5, blue: 0.5)
+                Text("Match this color")
+            }
+            VStack {
+                Color(red: 0.5, green: 0.5, blue: 0.5)
+                Text("R: 127  G: 127  B: 127")
+            }
+        }
+        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+            /*@START_MENU_TOKEN@*/Text("Button")/*@END_MENU_TOKEN@*/
+        })
     }
-
-  }
 }
+
 
 
 struct ContentView_Previews: PreviewProvider {

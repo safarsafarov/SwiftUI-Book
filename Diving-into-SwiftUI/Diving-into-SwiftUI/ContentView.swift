fe3f4043324f4e09b918b19709279b6f8d221365
safarsafarov <safarov.safar@hotmail.com>
2021-02-18 15:08:49 +0800

    commit ✨

--- Diving-into-SwiftUI/Diving-into-SwiftUI/ContentView.swift
+++ Diving-into-SwiftUI/Diving-into-SwiftUI/ContentView.swift
@@ -1,10 +1,3 @@
-//
-//  ContentView.swift
-//  Diving-into-SwiftUI
-//
-//  Created by codestorm on 2021/2/17.
-//
-
 import SwiftUI
 
 struct ContentView: View {
@@ -37,6 +30,7 @@ struct ContentView: View {
         }
     }
 }
+
 
 
 

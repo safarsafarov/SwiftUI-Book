50ad71e0d9c2a17654c3b4de692c5a3c5c2bcf87
safarsafarov <safarov.safar@hotmail.com>
2021-02-22 03:11:28 +0800

    commit

---
+++ Diving-into-SwiftUI/Diving-into-SwiftUI/TimeCounter.swift
@@ -0,0 +1,20 @@
+//
+//  TimeCounter.swift
+//  Diving-into-SwiftUI
+//
+//  Created by codestorm on 2021/2/27.
+//
+
+import SwiftUI
+
+struct TimeCounter: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TimeCounter_Previews: PreviewProvider {
+    static var previews: some View {
+        TimeCounter()
+    }
+}

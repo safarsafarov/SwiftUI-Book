8b6e309c038a003c59a150e68be71bae2e35c247
safarsafarov <safarov.safar@hotmail.com>
2021-02-28 01:09:42 +0800

    commit

--- Kuchi/Shared/Welcome/WelcomeView.swift
+++ Kuchi/Shared/Welcome/WelcomeView.swift
@@ -2,20 +2,27 @@ import SwiftUI
 
 struct WelcomeView: View {
     var body: some View {
-        Image(systemName: "table")
-            .resizable()
-            .frame(width: 30, height: 30)
-            // 1
-            .cornerRadius(30 / 2)
-            // 2
-            .overlay(Circle().stroke(Color.gray, lineWidth: 1))
-            // 3
-            .background(Color(white: 0.9))
-            // 4
-            .clipShape(Circle())
-            // 5
-            .foregroundColor(.red)
-
+        ZStack {
+            Image("welcome-background", bundle: nil)
+                .blur(radius: 5.0)
+            HStack{
+                Image(systemName: "table")
+                    .resizable()
+                    .frame(width: 30, height: 30)
+                    .overlay(Circle().stroke(Color.gray, lineWidth: 1))
+                    .background(Color(white: 0.9))
+                    .clipShape(Circle())
+                    .foregroundColor(.red)
+                
+                Text("Welcome to Kuchi")
+                    .font(.system(size: 30))
+                    .bold()
+                    .foregroundColor(.red)
+                    .lineLimit(2)
+                    .multilineTextAlignment(.leading)
+                
+            }
+        }
     }
 }
 

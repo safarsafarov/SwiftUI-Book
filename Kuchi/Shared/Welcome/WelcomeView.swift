a5f77a2e34f13e51767a83315397b5be2e97569c
safarsafarov <safarov.safar@hotmail.com>
2021-02-27 17:40:30 +0800

    commit

--- Kuchi/Shared/Welcome/WelcomeView.swift
+++ Kuchi/Shared/Welcome/WelcomeView.swift
@@ -2,7 +2,13 @@ import SwiftUI
 
 struct WelcomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Welcome to Kuchi")
+          .font(.system(size: 30))
+          .bold()
+          .foregroundColor(.red)
+          .lineLimit(2)
+          .multilineTextAlignment(.leading)
+
     }
 }
 
@@ -11,3 +17,4 @@ struct WelcomeView_Previews: PreviewProvider {
         WelcomeView()
     }
 }
+

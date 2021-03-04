c5a40e85f8772aeacd041ffe96337b2d5f6308f0
safarsafarov <safarov.safar@hotmail.com>
2021-03-05 05:30:31 +0800

    commit

--- Kuchi/Shared/Welcome/WelcomeView.swift
+++ Kuchi/Shared/Welcome/WelcomeView.swift
@@ -5,14 +5,7 @@ struct WelcomeView: View {
         ZStack {
             WelcomeBackgroundImage()
             HStack{
-                Image(systemName: "table")
-                    .resizable()
-                    .frame(width: 30, height: 30)
-                    .overlay(Circle().stroke(Color.gray, lineWidth: 1))
-                    .background(Color(white: 0.9))
-                    .clipShape(Circle())
-                    .foregroundColor(.red)
-                
+//                LogoImage()
                 VStack(alignment: .leading) {
                   Text("Welcome to")
                     .font(.headline)

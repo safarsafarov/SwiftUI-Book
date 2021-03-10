0ffe7a8680bf345530febdd8eb07877df53f5822
safarsafarov <safarov.safar@hotmail.com>
2021-03-10 20:13:27 +0800

    commit

--- Kuchi/Shared/Welcome/RegisterView.swift
+++ Kuchi/Shared/Welcome/RegisterView.swift
@@ -6,8 +6,20 @@ struct RegisterView: View {
     
     var body: some View {
         VStack {
+            Spacer()
             WelcomeMessageView()
             TextField("Type your name...", text: $name)
+                .padding(EdgeInsets(top: 8, leading: 16,
+                                    bottom: 8, trailing: 16))
+                .background(Color.white)
+                .overlay(
+                  RoundedRectangle(cornerRadius: 8)
+                    .stroke(lineWidth: 2)
+                    .foregroundColor(.blue)
+                )
+                .shadow(color: Color.gray.opacity(0.4),
+                        radius: 3, x: 1, y: 2)
+            Spacer()
         }
         .background(WelcomeBackgroundImage())
     }

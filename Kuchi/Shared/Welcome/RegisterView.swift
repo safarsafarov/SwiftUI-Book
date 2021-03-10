ca3248c732c22edcedfd859c62e6285051955077
safarsafarov <safarov.safar@hotmail.com>
2021-03-10 19:45:56 +0800

    commit

--- Kuchi/Shared/Welcome/RegisterView.swift
+++ Kuchi/Shared/Welcome/RegisterView.swift
@@ -7,6 +7,7 @@ struct RegisterView: View {
           WelcomeBackgroundImage()
           VStack {
             WelcomeMessageView()
+            TextField("Type your name...", text: $name)
           }
         }
     }

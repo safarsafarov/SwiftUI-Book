9011183a15e9f9ccb24f4a3e53822faf2e29b4c5
safarsafarov <safarov.safar@hotmail.com>
2021-03-11 20:17:13 +0800

    commit

--- Kuchi/Shared/Welcome/RegisterView.swift
+++ Kuchi/Shared/Welcome/RegisterView.swift
@@ -1,10 +1,21 @@
 import SwiftUI
 
 struct KuchiTextStyle: TextFieldStyle {
-  public func _body(
-    configuration: TextField<Self._Label>) -> some View {
+    public func _body(
+      configuration: TextField<Self._Label>) -> some View {
+
       return configuration
-  }
+        .padding(EdgeInsets(top: 8, leading: 16,
+                            bottom: 8, trailing: 16))
+        .background(Color.white)
+        .overlay(
+          RoundedRectangle(cornerRadius: 8)
+            .stroke(lineWidth: 2)
+            .foregroundColor(.blue)
+        )
+        .shadow(color: Color.gray.opacity(0.4),
+                radius: 3, x: 1, y: 2)
+    }
 }
 
 

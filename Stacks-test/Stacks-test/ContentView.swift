2e85a292bfe0ab014dfc256942a1cb0b0c126495
safarsafarov <safarov.safar@hotmail.com>
2021-03-29 20:17:19 +0800

    commit

---
+++ Stacks-test/Stacks-test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stacks-test
+//
+//  Created by codestorm on 2021/4/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Stacks-test/Stacks-test/Stacks_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stacks_testApp.swift
+//  Stacks-test
+//
+//  Created by codestorm on 2021/4/4.
+//
+
+import SwiftUI
+
+@main
+struct Stacks_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

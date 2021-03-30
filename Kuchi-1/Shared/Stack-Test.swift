7108eb103f565dafea40d673b3669044c3a57433
safarsafarov <safarov.safar@hotmail.com>
2021-03-30 20:33:05 +0800

    commit

--- Kuchi-1/Shared/Stack-Test.swift
+++ Kuchi-1/Shared/Stack-Test.swift
@@ -3,15 +3,15 @@ import SwiftUI
 struct Stack_Test: View {
     var body: some View {
         VStack{
+            Image("welcome-background")
+                .resizable()
             Text("A great and warm welcome to Kuchi")
                 .background(Color.red)
                 .frame(width: 100, height: 50, alignment: .center)
                 .minimumScaleFactor(0.5)
                 .background(Color.yellow)
-            Image("Welcome-kbacground")
-                .resizable()
-
         }
+        
     }
 }
 

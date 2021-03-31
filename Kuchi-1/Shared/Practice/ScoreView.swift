2779a94af793ac6deef1ac1f49eb740517014640
safarsafarov <safarov.safar@hotmail.com>
2021-03-31 20:42:57 +0800

    commit

--- Kuchi-1/Shared/Practice/ScoreView.swift
+++ Kuchi-1/Shared/Practice/ScoreView.swift
@@ -4,13 +4,21 @@ struct ScoreView: View {
     var numberOfAnswered = 0
     var numberOfQuestions = 5
     var body: some View {
-        
         HStack {
             Text("\(numberOfAnswered)/\(numberOfQuestions)")
-              .font(.caption)
-              .padding(4)
+                .font(.caption)
+                .padding(4)
             Spacer()
-          }
+        }
+        Button(action: {
+          self.showAnswers = !self.showAnswers
+        }) {
+          QuestionView(question: challengeTest.challenge.question)
+            .frame(height: 300)
+        }
+        // -Insert this-
+        ScoreView()
+
     }
 }
 

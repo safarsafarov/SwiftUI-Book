79dffc4a7c388fd716a88012ea4dbf4412b0143e
safarsafarov <safarov.safar@hotmail.com>
2021-03-31 20:45:53 +0800

    commit

--- Kuchi-1/Shared/Practice/ScoreView.swift
+++ Kuchi-1/Shared/Practice/ScoreView.swift
@@ -3,23 +3,26 @@ import SwiftUI
 struct ScoreView: View {
     var numberOfAnswered = 0
     var numberOfQuestions = 5
-    var body: some View {
-        HStack {
-            Text("\(numberOfAnswered)/\(numberOfQuestions)")
-                .font(.caption)
-                .padding(4)
-            Spacer()
-        }
-        Button(action: {
-          self.showAnswers = !self.showAnswers
-        }) {
-          QuestionView(question: challengeTest.challenge.question)
-            .frame(height: 300)
-        }
-        // -Insert this-
-        ScoreView()
-
+    struct State{
+        var numberOfAnswered = 0
     }
+    var state = State()
+    var body: some View {
+      // 1
+      Button(action: {
+        // 2
+        self.state.numberOfAnswered += 1
+      }) {
+        // 3
+        HStack {
+          Text("\(numberOfAnswered)/\(numberOfQuestions)")
+            .font(.caption)
+            .padding(4)
+          Spacer()
+        }
+      }
+    }
+
 }
 
 struct ScoreView_Previews: PreviewProvider {

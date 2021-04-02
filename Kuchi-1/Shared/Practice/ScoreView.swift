14dec201dc4e3aa414428a5a653d0d5e0496b02c
safarsafarov <safarov.safar@hotmail.com>
2021-04-02 22:22:05 +0800

    commit

--- Kuchi-1/Shared/Practice/ScoreView.swift
+++ Kuchi-1/Shared/Practice/ScoreView.swift
@@ -1,23 +1,25 @@
 import SwiftUI
 
 struct ScoreView: View {
-  var numberOfQuestions = 5
-
-  var _numberOfAnswered = State<Int>(initialValue: 0)
-
-  var body: some View {
-    Button(action: {
-      self._numberOfAnswered.wrappedValue += 1
-      print("Answered: \(self._numberOfAnswered.wrappedValue)")
-    }) {
-      HStack {
-        Text("\(_numberOfAnswered.wrappedValue)/\(numberOfQuestions)")
-          .font(.caption)
-          .padding(4)
-        Spacer()
-      }
+    var numberOfQuestions = 5
+    
+    @State
+    var numberOfAnswered = 0
+    
+    
+    var body: some View {
+        Button(action: {
+            self._numberOfAnswered.wrappedValue += 1
+            print("Answered: \(self._numberOfAnswered.wrappedValue)")
+        }) {
+            HStack {
+                Text("\(_numberOfAnswered.wrappedValue)/\(numberOfQuestions)")
+                    .font(.caption)
+                    .padding(4)
+                Spacer()
+            }
+        }
     }
-  }
 }
 
 

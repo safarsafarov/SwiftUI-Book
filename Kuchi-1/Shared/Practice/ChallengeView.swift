13e4ca7970e861b70d7fcc5e1d80de756cdcb647
safarsafarov <safarov.safar@hotmail.com>
2021-04-03 22:25:57 +0800

    commit

--- Kuchi-1/Shared/Practice/ChallengeView.swift
+++ Kuchi-1/Shared/Practice/ChallengeView.swift
@@ -1,43 +1,46 @@
 import SwiftUI
 
 struct ChallengeView: View {
-  let challengeTest: ChallengeTest
-  
-  @State var showAnswers = false
-  
-  var body: some View {
-    VStack {
-      Button(action: {
-        self.showAnswers.toggle()
-      }) {
-        QuestionView(question: challengeTest.challenge.question)
-          .frame(height: 300)
-      }
-      
-      if showAnswers {
-        Divider()
-        ChoicesView(challengeTest: challengeTest)
-          .frame(height: 300)
-          .padding()
-      }
+    let challengeTest: ChallengeTest
+    
+    @State var showAnswers = false
+    
+    var body: some View {
+        VStack {
+            Button(action: {
+                self.showAnswers = !self.showAnswers
+            }) {
+                QuestionView(question: challengeTest.challenge.question)
+                    .frame(height: 300)
+            }
+            // -Insert this-
+            ScoreView()
+            
+            
+            if showAnswers {
+                Divider()
+                ChoicesView(challengeTest: challengeTest)
+                    .frame(height: 300)
+                    .padding()
+            }
+        }
     }
-  }
 }
 
 
 struct ChallengeView_Previews: PreviewProvider {
-  // 1
-  static let challengeTest = ChallengeTest(
-    challenge: Challenge(
-      question: "おねがい　します",
-      pronunciation: "Onegai shimasu",
-      answer: "Please"
-    ),
-    answers: ["Thank you", "Hello", "Goodbye"]
-  )
-  
-  static var previews: some View {
-    // 2
-    return ChallengeView(challengeTest: challengeTest)
-  }
+    // 1
+    static let challengeTest = ChallengeTest(
+        challenge: Challenge(
+            question: "おねがい　します",
+            pronunciation: "Onegai shimasu",
+            answer: "Please"
+        ),
+        answers: ["Thank you", "Hello", "Goodbye"]
+    )
+    
+    static var previews: some View {
+        // 2
+        return ChallengeView(challengeTest: challengeTest)
+    }
 }
